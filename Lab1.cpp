8c6b66b1a488fc6a23a91f2ed81f5421f41b3b79
PARADOxxx1 <65506506+PARADOxxx1@users.noreply.github.com>
2023-10-12 02:27:17 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -19,6 +19,7 @@ void InsertionSort(vector <str> &a){
 
 int main()
 {
+    setlocale(LC_ALL, "rus");
     ifstream F;
     F.open("input1.txt", ios::in);
     vector <str> s1, s2;
@@ -36,14 +37,18 @@ int main()
         InsertionSort(s2);
     }
     F.close();
-    /*for (ll i = 0; i < s1.size(); i++) cout << s1[i] << " ";
-    cout << endl;
+    cout << "Упорядоченный список для файла input1.txt:\n";
+    for (ll i = 0; i < s1.size(); i++) cout << s1[i] << " ";
+    cout << endl << endl;
+    cout << "Упорядоченный список для файла input2.txt:\n";
     for (ll i = 0; i < s2.size(); i++) cout << s2[i] << " ";
-    cout << endl;*/
+    cout << endl << endl;
     vector <str> s = s1;
     for (ll i = 0; i < s2.size(); i++){
         s.push_back(s2[i]);
         InsertionSort(s);
     }
+    cout << "Единый упорядоченный список:\n";
     for (ll i = 0; i < s.size(); i++) cout << s[i] << " ";
+    cout << endl;
 }
