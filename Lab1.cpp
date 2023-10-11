8279ab9a66cd97146b5c11944d2e5da15b10808b
PARADOxxx1 <65506506+PARADOxxx1@users.noreply.github.com>
2023-10-11 23:38:34 +0300

    Add files via upload

---
+++ Lab1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+
+void InsertionSort(vector <str> &a){
+    ll ind = a.size() - 1;
+    str key = a[ind];
+    ll i = ind - 1;
+    while (i >= 0 && a[i] > key){
+        a[i + 1] = a[i];
+        i--;
+    }
+    a[i + 1] = key;
+}
+
+int main()
+{
+    ifstream F;
+    F.open("input1.txt", ios::in);
+    vector <str> s1, s2;
+    str x;
+    while (!F.eof()){
+        F >> x;
+        s1.push_back(x);
+        InsertionSort(s1);
+    }
+    F.close();
+    F.open("input2.txt", ios::in);
+    while (!F.eof()){
+        F >> x;
+        s2.push_back(x);
+        InsertionSort(s2);
+    }
+    F.close();
+    /*for (ll i = 0; i < s1.size(); i++) cout << s1[i] << " ";
+    cout << endl;
+    for (ll i = 0; i < s2.size(); i++) cout << s2[i] << " ";
+    cout << endl;*/
+    vector <str> s = s1;
+    for (ll i = 0; i < s2.size(); i++){
+        s.push_back(s2[i]);
+        InsertionSort(s);
+    }
+    for (ll i = 0; i < s.size(); i++) cout << s[i] << " ";
+}
